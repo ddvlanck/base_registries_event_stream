f7b194b42142b639494f885f22b3fdf6c7f944d0
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2021-11-04 15:35:04 +0100

    Update script

--- database/trim_street_names.sql
+++ database/trim_street_names.sql
@@ -1,2 +1,7 @@
+-- Updates every first element in array of column geographical name
+-- But sometimes array contains other items as well, so use with caution, because
+-- if used to update second element in array, and array contains only 1 element,
+-- it will return null.
+
 UPDATE brs.street_names
-SET geographical_name = jsonb_set("geographical_name"::jsonb->0, '{@value}', to_jsonb(trim(BOTH '"' FROM "geographical_name"::jsonb->0->>'@value')))
+SET geographical_name = jsonb_build_array(jsonb_set("geographical_name"::jsonb->0, '{@value}', to_jsonb(trim(BOTH '"' FROM "geographical_name"::jsonb->0->>'@value'))))
